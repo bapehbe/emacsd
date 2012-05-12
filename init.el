8ad87210fadbf7a4025662f9f7bec5c1955a1d0d
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-05-12 08:59:54 +0400

    more package sources

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/")
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
 (package-initialize)
 
 ; slime
